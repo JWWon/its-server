c5c98cbfa6157961c8dc14549cd4da635fc2d577
Jaeseong Seo <07js23@gmail.com>
2018-10-07 23:33:38 +0900

    Handle clinic not found

--- src/routes/clinics-api.js
+++ src/routes/clinics-api.js
@@ -1,9 +1,14 @@
 import { createController } from 'awilix-koa'
 import shortid from 'shortid'
+import { NotFound } from 'fejl'
 
 const api = Clinic => ({
   find: async ctx => ctx.ok(await Clinic.scan().exec()), // TODO deal with ctx.query
-  get: async ctx => ctx.ok(await Clinic.get(ctx.params.id)),
+  get: async ctx => {
+    const clinic = await Clinic.get(ctx.params.id)
+    NotFound.assert(clinic, 'Clinic not found')
+    ctx.ok(clinic)
+  },
   create: async ctx =>
     ctx.created(
       await Clinic.create({ ...ctx.request.body, id: shortid.generate() })
