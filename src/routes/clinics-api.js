24aabf3c3ba8b1f8478c91ac6df440c9fa2ad0b6
Jaeseong Seo <07js23@gmail.com>
2018-10-07 23:26:58 +0900

    Implement clinics api

---
+++ src/routes/clinics-api.js
@@ -0,0 +1,22 @@
+import { createController } from 'awilix-koa'
+import shortid from 'shortid'
+
+const api = Clinic => ({
+  find: async ctx => ctx.ok(await Clinic.scan().exec()), // TODO deal with ctx.query
+  get: async ctx => ctx.ok(await Clinic.get(ctx.params.id)),
+  create: async ctx =>
+    ctx.created(
+      await Clinic.create({ ...ctx.request.body, id: shortid.generate() })
+    ),
+  update: async ctx =>
+    ctx.ok(await Clinic.update({ id: ctx.params.id }, ctx.request.body)),
+  remove: async ctx => ctx.noContent(await Clinic.delete({ id: ctx.params.id }))
+})
+
+export default createController(api)
+  .prefix('/clinics')
+  .get('', 'find')
+  .get('/:id', 'get')
+  .post('', 'create')
+  .patch('/:id', 'update')
+  .delete('/:id', 'remove')
