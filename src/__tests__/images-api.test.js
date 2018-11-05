a8c52b9868c7cd3076ddd1d5f053a166e0200869
Jaeseong Seo <07js23@gmail.com>
2018-11-05 17:52:14 +0900

    Separate test suites for slide and news

--- src/__tests__/images-api.test.js
+++ src/__tests__/images-api.test.js
@@ -1,7 +1,7 @@
 import { apiHelper } from './api-helper'
 import { sample } from 'lodash'
 
-const data = {
+const commonData = {
   desktopSrc: sample([
     'http://static.hubzum.zumst.com/hubzum/2017/07/19/13/25ae58643b56453da421d17a4e42dbc5_780x0c.jpg',
     'https://kpopping.com/uploads/documents/mck_5afe1ebf24e9f.jpeg',
@@ -11,52 +11,84 @@ const data = {
     'http://www.ballplaya.com/xe/files/attach/images/22648/978/111/ff2014105fd46485f2eb2782db2394c6.png',
     'https://0.soompi.io/wp-content/uploads/2017/09/25015705/BTS-Steve-Aoki.jpg?s=900x600&e=t'
   ]),
-  type: sample(['slide', 'news']),
   alt: '이미지입니다'
 }
-let createdId
 
 describe('images API', () => {
-  it('can create image', async () => {
-    const api = await apiHelper(true)
-    await api.images.create({ ...data, type: 'slide' })
-    const image = await api.images.create(data)
+  describe('Slide', () => {
+    let createdId
+    const data = { ...commonData, type: 'slide' }
+    it('can create slide', async () => {
+      const api = await apiHelper(true)
+      const image = await api.images.create(data)
 
-    expect(image.id).toBeDefined()
-    createdId = image.id
-    expect(image.nonexistent).not.toBeDefined()
-    expect(image).toEqual(expect.objectContaining(data))
+      expect(image.id).toBeDefined()
+      createdId = image.id
+      expect(image.nonexistent).not.toBeDefined()
+      expect(image).toEqual(expect.objectContaining(data))
+    })
+
+    it('can find slides', async () => {
+      const api = await apiHelper()
+      const result = await api.images.find()
+      expect(result).toContainEqual(
+        expect.objectContaining({ ...data, id: createdId })
+      )
+    })
+
+    it('can remove slide', async () => {
+      const api = await apiHelper(true)
+      const image = await api.images.create(data)
+      await api.images.remove(image.id)
+
+      const result = await api.images.find()
+      expect(result).not.toContainEqual(
+        expect.objectContaining({ id: image.id })
+      )
+    })
+
+    it('can update slide', async () => {
+      const api = await apiHelper(true)
+      const newSrc =
+        'http://www.pennmike.com/news/photo/201803/3167_3201_3622.jpg'
+
+      const updated = await api.images.update(createdId, { desktopSrc: newSrc })
+
+      expect(updated.id).toBe(createdId)
+      expect(updated).toEqual(
+        expect.objectContaining({ ...data, desktopSrc: newSrc })
+      )
+    })
   })
 
-  it('can find images', async () => {
-    const api = await apiHelper()
-    const result = await api.images.find()
-    expect(result).toContainEqual(
-      expect.objectContaining({ ...data, id: createdId })
-    )
-  })
+  describe('News', () => {
+    let createdId
+    const data = {
+      ...commonData,
+      type: 'news',
+      title: 'News title',
+      content: 'this is content of news'
+    }
+    it('can create news', async () => {
+      const api = await apiHelper(true)
+      const image = await api.images.create(data)
 
-  it('can remove image', async () => {
-    const api = await apiHelper(true)
-    await api.images.remove(createdId)
+      expect(image.id).toBeDefined()
+      createdId = image.id
+      expect(image.nonexistent).not.toBeDefined()
+      expect(image).toEqual(expect.objectContaining(data))
+    })
 
-    const result = await api.images.find()
-    expect(result).not.toContainEqual(
-      expect.objectContaining({ ...data, id: createdId })
-    )
-  })
+    it('can update news', async () => {
+      const api = await apiHelper(true)
+      const newTitle = 'Hello world'
 
-  it('can update image', async () => {
-    const api = await apiHelper(true)
-    const created = await api.images.create(data)
-    const newSrc =
-      'http://www.pennmike.com/news/photo/201803/3167_3201_3622.jpg'
+      const updated = await api.images.update(createdId, { title: newTitle })
 
-    const updated = await api.images
-      .update(created.id, { desktopSrc: newSrc })
-      .catch(api.catch)
-
-    expect(updated.id).toBe(created.id)
-    expect(updated.desktopSrc).toBe(newSrc)
+      expect(updated.id).toBe(createdId)
+      expect(updated).toEqual(
+        expect.objectContaining({ ...data, title: newTitle })
+      )
+    })
   })
 })
