e0e00fd8125bba4fec8087dfbae146c7f58062c0
Jaeseong Seo <07js23@gmail.com>
2018-11-05 18:15:12 +0900

    Allow optional fields

--- src/lib/validate.js
+++ src/lib/validate.js
@@ -1,13 +1,16 @@
 import { chain } from 'lodash'
 
-export const isValid = (value, model) => {
-  if (typeof model === 'function' && typeof value === typeof model())
+export const isValid = (value, model, optional = false) => {
+  if (
+    (value === undefined && optional) ||
+    (typeof model === 'function' && typeof value === typeof model())
+  )
     return true
   return chain(value)
     .keys()
     .reduce(
       (result, k) =>
-        result && value[k] && model[k] && isValid(value[k], model[k]),
+        result && model[k] && isValid(value[k], model[k], optional),
       true
     )
     .value()
