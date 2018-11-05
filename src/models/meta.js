5f49efe9e53e9296da18a724427dcc52482bdcfb
Jaeseong Seo <07js23@gmail.com>
2018-11-05 17:33:07 +0900

    Add social and content columns on Meta

--- src/models/meta.js
+++ src/models/meta.js
@@ -1,4 +1,5 @@
 import dynamoose from 'dynamoose'
+import { isValid } from '../lib/validate'
 
 const Meta = dynamoose.model('Meta', {
   id: {
@@ -11,7 +12,19 @@ const Meta = dynamoose.model('Meta', {
   registration: String,
   email: String,
   phone: String,
-  address: String
+  address: String,
+  social: {
+    type: Object,
+    validate: s => {
+      const model = {
+        facebook: String,
+        instagram: String,
+        blog: String
+      }
+      return isValid(s, model)
+    }
+  },
+  content: String // 잇츠교정 소개
 })
 
 export default Meta
