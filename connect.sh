61260e57895eb79b9de8321bd9dce0ef1293deb9
Jaeseong Seo <07js23@gmail.com>
2018-11-11 22:20:22 +0900

    Modify ec2 dns

--- connect.sh
+++ connect.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i "its-server.pem" ubuntu@ec2-13-125-254-150.ap-northeast-2.compute.amazonaws.com
+ssh -i "its-server.pem" ubuntu@ec2-52-79-121-129.ap-northeast-2.compute.amazonaws.com
